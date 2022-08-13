7ec988694a9d87f82e6c7058a41017d3a281849c
KatrinaShr <katrineshr@gmail.com>
2022-08-13 23:10:25 +0300

    add: decode

--- src/index.js
+++ src/index.js
@@ -40,40 +40,40 @@ const MORSE_TABLE = {
 function decode(expr) {
     let array = expr.split('');
     let arrayDecimal = [];
+    let resultDec = [];
     let result = [];
-    let a = '.';
-    let b = '-';
-
+  
     for (let i=0; i<array.length; i+=10) {
-        let result = array.slice(i, i+10);
-        arrayDecimal.push(Number(result.join('')) || ' ');
-    } 
-
-    let array2 = arrayDecimal.map(x=> String(x));
-
-    for (let j=0; j<array2.length; j++) {
-
-        let k=0;
-        while (k<array2[j].length) {
-            if (array2[j][k] + array2[j][k+1]  === '10') { result.push.('.')}
-            if (array2[j][k] + array2[j][k+1]  === '10') { result.push('.')}
-            else if (array2[j][k] + array2[j][k+1]  === '11') {result.push('-')}
-            else ' ';
-            k++;
-            result.join('');
-        }
-        
-        // for (let k=0; k<array2[j].length; k+=2) {
-        //   array2[j]  === ' ' ? find.push(' ') : (array2[j][k] + array2[j][k+1]  === '10' ? find.push('.') : find.push('-'));  
-        // }  
+        arrayDecimal.push(array.slice(i, i+10)); //разделение элем. по 10 в массиве
+  
+        for (let j=0; j<arrayDecimal.length; j++) {
+            while (arrayDecimal[j][0] === '0' || arrayDecimal[j][0] === '*' ) { //удаление нулей слева
+              arrayDecimal[j].shift(); 
+              arrayDecimal.join(',');
+            }
+          }
     }
-   
-    return result;
-    //return arrayDecimal;
+     for (let i=0; i<arrayDecimal.length; i++) {
+        let findString = '';
+         for (let j=0; j<arrayDecimal[i].length; j+=2) {
+            
+            if (arrayDecimal[i][j] === '') {
+                findString += ' ';
+            }
+            
+            else if (arrayDecimal[i][j] + arrayDecimal[i][j+1] === '10') { 
+                findString += '.';
+         }
+             else if (arrayDecimal[i][j] + arrayDecimal[i][j+1] === '11') { 
+                findString += '-';}
+     }
+     resultDec.push(findString);
+      }
+
+     resultDec.map(item => {return result += ((MORSE_TABLE[item]) || ' ')});
+     return result;
     }
 
-console.log(decode("0000101010000000101100101010110000000010**********00000011110000000010"))
-
-module.exports = {
+    module.exports = {
     decode
 }
