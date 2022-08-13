91a05fffe4f18839090a3486978a234e8ad265db
KatrinaShr <katrineshr@gmail.com>
2022-08-13 11:25:55 +0300

    fix: func

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let array = expr.split('');
+    let arrayDecimal = [];
+    let result = [];
+    let a = '.';
+    let b = '-';
+
+    for (let i=0; i<array.length; i+=10) {
+        let result = array.slice(i, i+10);
+        arrayDecimal.push(Number(result.join('')) || ' ');
+    } 
+
+    let array2 = arrayDecimal.map(x=> String(x));
+
+    for (let j=0; j<array2.length; j++) {
+
+        let k=0;
+        while (k<array2[j].length) {
+            if (array2[j][k] + array2[j][k+1]  === '10') { result.push.('.')}
+            if (array2[j][k] + array2[j][k+1]  === '10') { result.push('.')}
+            else if (array2[j][k] + array2[j][k+1]  === '11') {result.push('-')}
+            else ' ';
+            k++;
+            result.join('');
+        }
+        
+        // for (let k=0; k<array2[j].length; k+=2) {
+        //   array2[j]  === ' ' ? find.push(' ') : (array2[j][k] + array2[j][k+1]  === '10' ? find.push('.') : find.push('-'));  
+        // }  
+    }
+   
+    return result;
+    //return arrayDecimal;
+    }
+
+console.log(decode("0000101010000000101100101010110000000010**********00000011110000000010"))
 
 module.exports = {
     decode
