5c1dbaefde76dffd557c55d481006b849ee65cdc
KatrinaShr <katrineshr@gmail.com>
2024-10-20 15:15:05 +0300

    init: start morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,42 +38,9 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    let array = expr.split('');
-    let arrayDecimal = [];
-    let resultDec = [];
-    let result = [];
-  
-    for (let i=0; i<array.length; i+=10) {
-        arrayDecimal.push(array.slice(i, i+10)); //разделение элем. по 10 в массиве
-  
-        for (let j=0; j<arrayDecimal.length; j++) {
-            while (arrayDecimal[j][0] === '0' || arrayDecimal[j][0] === '*' ) { //удаление нулей слева
-              arrayDecimal[j].shift(); 
-              arrayDecimal.join(',');
-            }
-          }
-    }
-     for (let i=0; i<arrayDecimal.length; i++) {
-        let findString = '';
-         for (let j=0; j<arrayDecimal[i].length; j+=2) {
-            
-            if (arrayDecimal[i][j] === '') {
-                findString += ' ';
-            }
-            
-            else if (arrayDecimal[i][j] + arrayDecimal[i][j+1] === '10') { 
-                findString += '.';
-         }
-             else if (arrayDecimal[i][j] + arrayDecimal[i][j+1] === '11') { 
-                findString += '-';}
-     }
-     resultDec.push(findString);
-      }
+    // write your solution here
+}
 
-     resultDec.map(item => {return result += ((MORSE_TABLE[item]) || ' ')});
-     return result;
-    }
-
-    module.exports = {
+module.exports = {
     decode
 }
